10386d91b3ab13568ad0250bcf28c2d949d58a19
deeej <deeej@192.168.1.2>
2018-11-27 12:27:44 +0000

    Telusko

---
+++ Codility/src/com/assignment/PatternStar.java
@@ -0,0 +1,47 @@
+package com.assignment;
+
+public class PatternStar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i =0; i<=4; i++)
+		{
+			for(int j=0; j<=4; j++)
+			
+				System.out.print("* ");
+			    
+			System.out.println();
+
+			
+		}
+		
+		
+		for(int i =1; i<=7; i++)
+		{
+			for(int j=1; j<i; j++)
+			
+				System.out.print(j+" ");
+			    
+			System.out.println();
+
+			
+		}
+		
+		for(int i =0; i<=4; i++)
+		{
+			int alphabet = 65;
+			for(int j=0; j<i; j++)
+			{
+				System.out.print((char)alphabet+" ");
+				alphabet++;
+			}   
+			System.out.println();
+			
+			
+		}
+		
+		
+	}
+
+}
